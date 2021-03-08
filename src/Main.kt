b8830a8b61d7ca605bf3223dd14bfbee0a1ab654
William Dahl <williamad90@gmail.com>
2021-03-08 18:25:16 +0100

    small exaple of mutable / immutable and top-level variables

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,18 @@
+/* Top-level variables */
+var email: String = "mail@mail"
+
 fun main() {
-    println("Hello Coronaworld")
+
+    /*Mutable variable*/
+    var firstName: String = "Aggatha"
+
+    /*Immutable variable*/
+    val surName: String = "Mars"
+    firstName = "Nataile"
+
+    email = "@gmail.com"
+    println(firstName)
+    println(email)
+
+
 }
