81f66f4ed9a7eaba7898277996724e51c02c4bde
William Dahl <williamad90@gmail.com>
2021-03-08 18:36:52 +0100

    small exapmle of types, type inference and nullable variables

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,21 @@
+/* Types are non-null by default*/
+/* If you want to use null initialized variables use the '?' on type ( nullable) */
+var greeting: String? = "hello"
+val name: String = "Natalie"
+
+/* Kotlin supports type inferences. Var and Val can therefore be declared as such : */
+var mail = "mail@mail"
+
 fun main() {
-    println("Hello Coronaworld")
+    println(greeting)
+    println(name)
+
+    greeting = null
+
+    println(greeting)
+    println(name)
+
+    println(mail)
+
+
 }
