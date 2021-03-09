e54e0c88ecddaee884ecf16f489793b62fffcc1e
William Dahl <williamad90@gmail.com>
2021-03-09 19:23:45 +0100

    flow controll examples with if/else and when

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,32 @@
-/* Kotlin variables are non-null by default
-*  If we want nullable variables add '?' after type
-*  Kotlin supports type inference on variables and properties!
-* */
 
-var email = "mail@mail"
-var name: String? = "Natalie"
+var greeting: String? = null
 
 fun main() {
 
-    email = "@gmail.com"
-    name = null
-    println(name)
-    println(email)
+    /* standard if / else */
+    if(greeting != null){
+        println(greeting)
+    }else{
+        println("hi")
+    }
+    println("--------------")
 
+    /* when statement is much like switch statement */
+    when(greeting){
+        null -> println("hi")
+        else -> println(greeting)
+    }
+    println("--------------")
+    /* If greeting has some value, the val vil be set to greeting.*/
+    val greetingToPrint = if(greeting != null) greeting else "I am greetingToPrint, hi"
+    println(greetingToPrint)
+    println("--------------")
 
+    greeting = "Hello"
+
+    val secondGreeting = when(greeting){
+        null -> "hi"
+        else -> greeting
+    }
+    println(secondGreeting)
 }
