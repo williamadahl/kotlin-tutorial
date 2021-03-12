5daf117fbb58f1e084d325d45c50284bf85fb7a7
William Dahl <williamad90@gmail.com>
2021-03-12 16:01:46 +0100

    semi-java similar iteration of array'

--- src/Main.kt
+++ src/Main.kt
@@ -1,22 +1,13 @@
-/*
-* Kotlin supports string templates, so in stead of concatenation "+" we can use "$"
-* Here are some top-level function examples
-* */
-
-fun sayHello(itemToPrint:String){
-    val msg = "Hello " + itemToPrint
-    val msgTemplate = "With string template: Hello $itemToPrint"
-    println(msg)
-    println(msgTemplate)
-}
-// Here as a single expression function:
-fun sayHelloSingleExpression (itemToPrint: String) = println("Hello Kotlin $itemToPrint")
 
 fun twoParams(greetingString:String, greetingTo:String) = println("$greetingString $greetingTo")
 
 
 fun main() {
-    sayHello("Kotlin")
-    sayHelloSingleExpression("with single expression")
-    twoParams("Hello", "my friend")
+    val listOfThings = arrayOf("Kotlin", "Java", "Python")
+    println("Size ${listOfThings.size}")
+    println("First item with indexing: " + listOfThings[0])
+    println("First item with get: ${listOfThings.get(0)}")
+    for(things in listOfThings){
+        println(things)
+    }
 }
