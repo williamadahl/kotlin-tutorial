01feeefe5479030387da092f0b6fbe4f51a29c0b
William Dahl <williamad90@gmail.com>
2021-03-12 18:10:09 +0100

    some examples working with maps and mutable collections

--- src/Main.kt
+++ src/Main.kt
@@ -1,27 +1,29 @@
 
-fun twoParams(greetingString:String, greetingTo:String) = println("$greetingString $greetingTo")
+fun sayHello(greetingString: String, greetingTo:List<String>) = run {
+    greetingTo.forEach{ personToGreet ->
+        println("$greetingString, $personToGreet")
+    }
+}
 
 
 fun main() {
-    val listOfThings = arrayOf("Kotlin", "Java", "Python")
-    println("Size ${listOfThings.size}")
-    println("First item with indexing: " + listOfThings[0])
-    println("First item with get: ${listOfThings.get(0)}")
-    for(things in listOfThings){
-        println(things)
-    }
-    /* More functional :
-    *  Remember the arrow :)
-    *  This is lambda syntax in Kotlin
-    *  */
-    listOfThings.forEach {thing ->
+    val listOfThings = listOf("Kotlin", "Java", "Python")
+    listOfThings.forEach { thing ->
         println(thing)
     }
+    /* Map takes in pairs
+    *  By default a collection type in kotlin is immutable
+    * */
+    val map = mapOf(1 to "Kotlin", 2 to "Java", 3 to "Python")
+    map.forEach{ (key, value) -> println("$key -> $value")}
 
     /*
-    * Can also used an indexed function for this:
+    * Here is an mutable example
     * */
-    listOfThings.forEachIndexed { index, thing ->
-        println("$index : $thing")
-    }
+
+    val mutableMap = mutableMapOf(1 to "Nina", 2 to "Natalie", 3 to "Lexi")
+    mutableMap.put(4, "Nadia")
+    mutableMap.forEach{ (key, value) -> println("$key -> $value")}
+
+    sayHello("hello", listOfThings)
 }
